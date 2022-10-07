16205660b86bbd70770ec70fb094d8ef1506839b
RNagam <101287150+RNagam@users.noreply.github.com>
2022-10-07 06:59:39 +0100

    Add files via upload

---
+++ MarsRoverNasa/MarsRoverNasa/Model/PlataueCommandExecuter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MarsRover
+{
+    public class PlataueCommandExecuter : CommandExecuter
+    {
+        private readonly ILandingSurface _landingSurface;
+
+        public PlataueCommandExecuter(IServiceProvider serviceProvider)
+        {
+            _landingSurface = serviceProvider.GetService<ILandingSurface>();
+        }
+
+        public override Regex RegexCommandPattern => new Regex("^\\d+ \\d+$");
+
+        public override string ExecuteCommand(string command)
+        {
+            ParseCommand(command, out var width, out var height);
+            _landingSurface.Define(width, height);
+            return string.Empty;
+        }
+
+        private static void ParseCommand(string command, out int width, out int height)
+        {
+            var splitCommand = command.Split(' ');
+            width = int.Parse(splitCommand[0]) + 1;
+            height = int.Parse(splitCommand[1]) + 1;
+        }
+    }
+}
